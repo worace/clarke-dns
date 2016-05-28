65315322993ba67a3c32d27d0b1643e4f50cbb7c
Horace Williams <horace.d.williams@gmail.com>
2016-05-28 12:05:17 -0600

    read port for ENV to support heroku

--- src/clarke_dns/core.clj
+++ src/clarke_dns/core.clj
@@ -4,12 +4,15 @@
             [org.httpkit.server :as httpkit]
             [clarke-dns.peer-validation :as pv]))
 
+(defn env [key]
+  (System/getenv key))
+
 (defonce server-shutdown-fn (atom nil))
 (def update-delay 5000) ;; ms
-(def port 3000)
+
 (defn -main [& args]
   (println "Starting clarke DNS server.")
-  (reset! server-shutdown-fn (httpkit/run-server app {:port port}))
+  (reset! server-shutdown-fn (httpkit/run-server app {:port (or (env "PORT") 3000)}))
   (.addShutdownHook (Runtime/getRuntime)
                     (Thread. #((println "Stopping Clarke DNS Web Server")
                                (@server-shutdown-fn 100))))
